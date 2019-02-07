f9e099604f4a95b85b66692bfeba56f7624b3f13
chenjiena <chenjiena@meituan.com>
2019-02-07 22:44:12 +0800

    first

---
+++ flyingSquirrel.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <set> 
+ 
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int age;
+    cin>>age;
+    cout<<age;
+    return 0;
+}
+
